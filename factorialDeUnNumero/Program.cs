2f770a5175f5a8600593926ff6d5e464be7f12bf
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:49:52 -0300

    FACTORIAL DE UN NÚMERO

---
+++ factorialDeUnNumero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace FACTORIAL_DE_UN_NÚMERO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte NUM, K;
+            long RESUL = 1;
+            string linea;
+            Console.Write("DIGITE UN NÚMERO: "); linea = Console.ReadLine();
+            NUM = byte.Parse(linea);
+            for (K = 2; K <= NUM; K++)
+            {
+                RESUL = RESUL * K;
+            }
+            Console.WriteLine("EL FACTORIAL ES: " + RESUL);
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
